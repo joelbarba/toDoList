06b3beb724a71ccbf906956c9c5b66def31dd493
joelbarba <joel.barba@blueface.com>
2016-07-14 17:46:10 +0100

    First crud working

--- client/main.js
+++ client/main.js
@@ -1,19 +1,77 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
- 
+
 angular.module('toDoList', [angularMeteor])
-.controller('MainCtrl', 
+.controller('MainCtrl',
 	function($scope, $reactive) {
 		'ngInject';
 		// $scope.var1 = 'Hello world';
 
+    $scope.detMode = 'show';
+
     $reactive(this).attach($scope);
-    
+
     this.helpers({
       tasks() {
         return AllTasks.find({});
       }
     });
+
+    // Object to handle actions (add / edit / delete) to one item of the list
+    $scope.itemHandler = (function() {
+      var itemHandler = {};
+
+      itemHandler.task = {};
+
+      // Prepare the panel to add a new item
+      itemHandler.showAddPanel = function() {
+        $scope.detMode = 'add';
+
+      };
+
+      // Prepare the panel to edit a new item
+      itemHandler.showEditPanel = function(task) {
+        $scope.detMode = 'edit';
+        this.task = angular.copy(task);
+        this.listItemReference = task;
+      };
+
+      // Add new item
+      itemHandler.add = function() {
+
+        AllTasks.insert(this.task);
+        // AllTasks.insert({
+        //   name        : this.task.name,
+        //   description : this.task.description,
+        //   done        : this.task.done
+        // });
+        $scope.itemHandler.showEditPanel(this.task);
+      };
+
+      // Remove an item
+      itemHandler.remove = function() {
+        AllTasks.remove({ _id: this.task._id });
+        this.cancel();
+      };
+
+      // Update an item
+      itemHandler.update = function() {
+        AllTasks.update(
+          {  _id: this.task._id },
+          { $set: task }
+        );
+      };
+
+      // Cancel the details panell
+      itemHandler.cancel = function() {
+        $scope.detMode = 'show';
+        this.task = {};
+      };
+
+      return itemHandler;
+
+    }());
+
 
 	}
 );
--- server/main.js
+++ server/main.js
@@ -4,14 +4,14 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
 
 	// Meteor.users.remove({},{justOne: false});	// Truncate collection
-	// AllTasks.remove({},{justOne: false});		// Truncate collection
+	AllTasks.remove({},{justOne: false});		// Truncate collection
   	
   	if (!AllTasks.findOne()) {
   		var dataIni = [
-  			{ name: 'Task1', done: false },
-  			{ name: 'Task2', done: true  },
-  			{ name: 'Task3', done: false },
-  			{ name: 'Task4', done: false }
+  			{ name: 'Task1', description: 'this is a tough task 1', done: false },
+  			{ name: 'Task2', description: 'this is a tough task 2', done: true  },
+  			{ name: 'Task3', description: 'this is a tough task 3', done: false },
+  			{ name: 'Task4', description: 'this is a tough task 4', done: false }
   		];
 
 		  dataIni.forEach(function(task) { AllTasks.insert(task); });
